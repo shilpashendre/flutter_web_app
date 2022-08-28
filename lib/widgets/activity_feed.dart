8bffe664e48a4f77807dca61fa125ceaec9745af
Shilpa-Sage <shilpa.shendre@sage.com>
2022-08-28 21:01:16 +0530

    minor fix

--- lib/widgets/activity_feed.dart
+++ lib/widgets/activity_feed.dart
@@ -13,74 +13,68 @@ class ActivityFeed extends StatelessWidget {
     return ListView.builder(
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
-        return Wrap(
-          children: [
-            SizedBox(
-              height: 150,
-              width: 250,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Positioned(
-                    height: 150,
-                    width: 200,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 30.0),
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          color: cardPinkColor,
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                        ),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(top: 18.0),
-                                child: Text(
-                                  dashboardMenu[index].title,
-                                  style: AppTextTheme.cardText,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              Container(
-                                  decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(5))),
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 30.0, vertical: 6),
-                                    child: Text(
-                                      dashboardMenu[index].value,
-                                      textAlign: TextAlign.center,
-                                      style: const TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w600,
-                                          fontSize: 16),
-                                    ),
-                                  ))
-                            ]),
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: SizedBox(
+            height: 150,
+            width: 200,
+            child: Stack(
+              children: [
+                Positioned(
+                  height: 150,
+                  width: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: cardPinkColor,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
                       ),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 18.0),
+                              child: Text(
+                                dashboardMenu[index].title,
+                                style: AppTextTheme.cardText,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Container(
+                                decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(5))),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 30.0, vertical: 6),
+                                  child: Text(
+                                    dashboardMenu[index].value,
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 16),
+                                  ),
+                                ))
+                          ]),
                     ),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Positioned(
-                        height: 70,
-                        width: 70,
-                        child: Image.asset(
-                          dashboardMenu[index].imagePath,
-                          height: 70,
-                          width: 70,
-                        ),
-                      ),
-                    ],
+                ),
+                Positioned(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Image.asset(
+                      dashboardMenu[index].imagePath,
+                      height: 70,
+                      width: 70,
+                    ),
                   ),
-                ],
-              ),
-            )
-          ],
+                ),
+              ],
+            ),
+          ),
         );
       },
       itemCount: dashboardMenu.length,
